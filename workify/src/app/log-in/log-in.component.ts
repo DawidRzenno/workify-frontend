9e4dad9710588c23b3f6c9c791bbf0056baefb7b
Dawid Rzenno <dawid.rzenno@protonmail.com>
2021-02-20 17:11:18 +0100

    Generate and implement LoginComponent

--- workify/src/app/log-in/log-in.component.ts
+++ workify/src/app/log-in/log-in.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {LogInBody} from '../model/log-in-body.model';
+import {LogInService} from './log-in.service';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.scss']
 })
-export class LogInComponent implements OnInit {
+export class LogInComponent {
+  logInForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private logInService: LogInService) {
+    this.logInForm = formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      rememberMe: [{value: false, disabled: true}]
+    });
   }
 
+  get email(): AbstractControl | null {
+    return this.logInForm.get('email');
+  }
+
+  get password(): AbstractControl | null {
+    return this.logInForm.get('password');
+  }
+
+  logIn(): void {
+    const body = new LogInBody(this.email?.value, this.password?.value);
+    this.logInService.logIn(body).subscribe(
+      response => console.log(response.body));
+  }
 }
