685d8bf753b1c0ce27b9345396e2df9b1f45a707
Dawid Rzenno <dawid.rzenno@protonmail.com>
2021-02-23 20:47:14 +0100

    Generate and implement ProjectComponent. Create Project model

---
+++ workify/src/app/components/project/project.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { IProject } from '../../models/project.model';
+import { TimeTrackerService } from '../time-tracker/time-tracker.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.scss'],
+})
+export class ProjectComponent implements OnInit {
+  project: IProject | null;
+
+  constructor(
+    private timeTrackerService: TimeTrackerService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.project = null;
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      if (params.id) {
+        this.project = this.timeTrackerService.getProjectById(params.id);
+      }
+    });
+  }
+}
--- workify/src/app/models/project.model.ts
+++ workify/src/app/models/project.model.ts
@@ -4,16 +4,19 @@ export interface IProject {
   id: string;
   name: string;
   entries: IEntry[];
+  userId: string;
 }
 
 export class Project implements IProject {
   id: string;
   name: string;
   entries: IEntry[];
+  userId: string;
 
-  constructor(id: string, name: string, entries: IEntry[]) {
+  constructor(id: string, name: string, entries: IEntry[], userId: string) {
     this.id = id;
     this.name = name;
     this.entries = entries;
+    this.userId = userId;
   }
 }
